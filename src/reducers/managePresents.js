9739616e9642f405ea1f7948fed85710863c52d1
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-09 17:10:58 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,8 @@
 export function managePresents(state, action){
+  switch (action.type) {
+  case 'INCREASE':
+    return {...state, presents: state.presents + 1}
+  default:
+    return state;
+  }
 }
